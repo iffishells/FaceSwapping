239d6cb980c048fa39954359047736fcd6be1395
iffishells <iffishells@gmail.com>
2024-03-09 18:53:40 +0500

    added insight env setup

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import insightface
+from insightface.app import FaceAnalysis
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+    # image_path = ''
+    # read_image()
